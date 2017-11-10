e7e024bbc4d9ceee0c094e7e25351fe98895189b
hydrater <liquid_assassin@hotmail.com>
2017-11-10 21:59:59 +0800

    Added game

---
+++ Assets/000/Menu Scene/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Projectile : NetworkBehaviour {
+
+	float timeDelta;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate(Vector3.forward * Time.deltaTime * 5);
+		timeDelta += Time.deltaTime;
+		if (timeDelta > 8) Network.Destroy(gameObject);
+	}
+
+	void OnTriggerEnter3D(Collider other)
+	{
+		NetworkPlayer temp;
+		if ((temp = other.GetComponent<NetworkPlayer>()) != null)
+		{
+			--temp.hp;
+		}
+
+		if (other.tag == "FireWall")
+		{
+			if (gameObject.tag == "Ice")
+			{
+				Network.Destroy(gameObject);
+			}
+		}
+
+		if (other.tag == "IceWall")
+		{
+			if (gameObject.tag == "Fire")
+			{
+				Network.Destroy(gameObject);
+			}
+		}
+
+	}
+}
--- Assets/NetworkPlayer.cs
+++ Assets/NetworkPlayer.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class NetworkPlayer : MonoBehaviour {
+public class NetworkPlayer : NetworkBehaviour {
 
+	[SyncVar]
+	public int hp = 10;
 
+	CubeGestureListener gestureListener;
+	public GameObject[] gameplayObj;
+
+	bool canWall;
 
 	// Use this for initialization
 	void Start () 
 	{
+		gestureListener = CubeGestureListener.Instance;
+
 		if (!GetComponent<NetworkIdentity>().hasAuthority)
 		{
 			GetComponent<AvatarController>().enabled = false;
@@ -25,6 +33,43 @@ public class NetworkPlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		if (gestureListener.IsSwipeUp())
+		if (canWall) StartCoroutine(wallUp(gameplayObj[0]));
+
+		if (gestureListener.IsSwipeDown())
+		if (canWall) StartCoroutine(wallUp(gameplayObj[1]));
+
+		if (gestureListener.IsSwipeLeft())
+		{
+			NetworkServer.Spawn(gameplayObj[3]);
+		}
+
+		if (gestureListener.IsSwipeRight())
+		{
+			NetworkServer.Spawn(gameplayObj[4]);
+		}
 	}
+
+	IEnumerator wallUp(GameObject wall)
+	{
+		canWall = false;
+		do
+		{
+			wall.transform.Translate(Vector3.up * Time.deltaTime * 5);
+			yield return null;
+		}while (wall.transform.position.y >= 1.3);
+
+		yield return new WaitForSeconds(0.6f);
+
+		do
+		{
+			wall.transform.Translate(-Vector3.up * Time.deltaTime * 5);
+			yield return null;
+		}while (wall.transform.position.y <= 1.6);
+
+		canWall = true;
+	}
+
+
+
 }
